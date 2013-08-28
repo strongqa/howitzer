4ca1f8346ab5ac758b9136543aef3c01a8ae1076
trofymenko <oleksandr.trofymenko@gmail.com>
2013-08-28 13:47:10 +0200

    refactored data_storage_spec.rb

--- spec/unit/lib/data_storage_spec.rb
+++ spec/unit/lib/data_storage_spec.rb
@@ -3,12 +3,14 @@ require_relative '../../../lib/howitzer/utils/data_generator/data_storage'
 
 describe "DataGenerator" do
   describe "DataStorage" do
-    describe "#store" do
+    describe ".store" do
       subject { DataGenerator::DataStorage.store(ns, 7, :halt) }
       context "when namespace specified" do
         let(:ns) { :user }
-        it do
+        it "should return value" do
           expect(subject).to eql(:halt)
+        end
+        it "should store namespace value" do
           adata = DataGenerator::DataStorage.instance_variable_get(:@data)
           expect(adata[:user]).to eql({7 => :halt})
         end
@@ -18,31 +20,43 @@ describe "DataGenerator" do
         it { expect {subject}.to raise_error(RuntimeError, "Data storage namespace can not be empty") }
       end
     end
-    describe "#extract" do
-      subject { DataGenerator::DataStorage.extract(:user, key) }
+    describe ".extract" do
+      subject { DataGenerator::DataStorage.extract(ns, key) }
       before { DataGenerator::DataStorage.instance_variable_set(:@data, {user: {7 => :exit}}) }
-      context "when namespace key found" do
-        let(:key) { 7 }
-        it { expect(subject).to eql(:exit) }
+      describe "when namespace specified" do
+        let(:ns) { :user }
+        context "and namespace key found" do
+          let(:key) { 7 }
+          it { expect(subject).to eql(:exit) }
+        end
+        context "and namespace key not found" do
+          let(:key) { 5 }
+          it { expect(subject).to be_nil }
+        end
+        context "but namespace key not specified" do
+          let(:key) { nil }
+          it { expect(subject).to eql({ 7 => :exit }) }
+        end
       end
-      context "when namespace key not found" do
-        let(:key) { 5 }
-        it { expect(subject).to eql(nil) }
+      context "when namespace not found" do
+        let(:ns) { :guest }
+        let(:key) { 11 }
+        it { expect(subject).to be_nil }
       end
-      context "when namespace key not specified" do
+      context "when namespace not specified" do
+        let(:ns) { nil }
         let(:key) { nil }
-        it { expect(subject).to eql({ 7 => :exit }) }
+        it { expect {subject}.to raise_error(RuntimeError, "Data storage namespace can not be empty") }
       end
     end
-    describe "#clear_ns" do
+    describe ".clear_ns" do
       subject { DataGenerator::DataStorage.clear_ns(:user) }
       before { DataGenerator::DataStorage.instance_variable_set(:@data, {user: {7 => :exit}}) }
-      it do
-        expect(subject).to eql({})
+      it "should return empty hash" do
+        subject
         adata = DataGenerator::DataStorage.instance_variable_get(:@data)
         expect(adata[:user]).to eql({})
       end
-
     end
   end
 end
