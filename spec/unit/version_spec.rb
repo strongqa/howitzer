ef8f77ed4acbeb16feca37235595a1281a83b6fe
Roman Parashchenko <romikoops1@gmail.com>
2013-10-06 22:36:25 +0300

    Fixed test

--- spec/unit/version_spec.rb
+++ spec/unit/version_spec.rb
@@ -3,6 +3,6 @@ require "spec_helper"
 describe Howitzer do
   it { expect(subject.constants).to include(:VERSION) }
   it 'should contains VERSION constant with correct format' do
-    expect(Howitzer::VERSION).to match(/^\d+\.\d+\.\d+$/)
+    expect(Howitzer::VERSION).to match(/^\d+\.\d+(\.\d+)?$/)
   end
 end
