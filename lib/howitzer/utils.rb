6fd9d1e3501ac20f0cc99cdb3014f14199b88dc7
nikolay <zozulyak.nick@gmail.com>
2014-04-05 14:11:19 +0300

    fix bug

--- lib/howitzer/utils.rb
+++ lib/howitzer/utils.rb
@@ -1,5 +1,6 @@
 require 'repeater'
 require 'capybara'
 require 'selenium-webdriver'
+require 'active_support/core_ext'
 
 Dir[File.join(File.dirname(__FILE__), "./utils/**/*.rb")].each {|f| require f}
