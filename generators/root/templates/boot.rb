e5fa49af738d4ee75b8e335e971bebfb5f4f7832
Roman Parashchenko <romikoops1@gmail.com>
2015-08-22 20:59:06 +0200

    Fixes

--- generators/root/templates/boot.rb
+++ generators/root/templates/boot.rb
@@ -9,6 +9,6 @@ require 'howitzer'
 Dir[File.join(File.dirname(__FILE__), "./emails/**/*.rb")].each {|f| require f}
 Dir[File.join(File.dirname(__FILE__), "./pages/**/*.rb")].each {|f| require f}
 
-Dir[File.join(File.dirname(__FILE__), "./prerequisites/models/**/*.rb")].each {|f| require f}
 require File.join(File.dirname(__FILE__), "prerequisites/her")
+Dir[File.join(File.dirname(__FILE__), "./prerequisites/models/**/*.rb")].each {|f| require f}
 require File.join(File.dirname(__FILE__), "prerequisites/factory_girl")
--- spec/unit/lib/utils/gen_spec.rb
+++ spec/unit/lib/utils/gen_spec.rb
@@ -1,20 +1,18 @@
 require 'spec_helper'
 require 'howitzer/utils/gen'
 
-RSpec.describe 'DataGenerator' do
-  describe 'Gen' do
-    describe '.serial' do
-      subject { Gen.serial }
-      context 'received value should conform to template' do
-        it { is_expected.to match /\d{9}\w{5}/ }
-      end
+RSpec.describe 'Gen' do
+  describe '.serial' do
+    subject { Gen.serial }
+    context 'received value should conform to template' do
+      it { is_expected.to match /\d{9}\w{5}/ }
     end
+  end
 
-    describe '#serial' do
-      subject { double.tap{ |s| s.extend(Gen)}.serial }
-      context 'received value should conform to template' do
-        it { is_expected.to match /\d{9}\w{5}/ }
-      end
+  describe '#serial' do
+    subject { double.tap{ |s| s.extend(Gen)}.serial }
+    context 'received value should conform to template' do
+      it { is_expected.to match /\d{9}\w{5}/ }
     end
   end
 end
