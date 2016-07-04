837ab10d92fb7fba095ec65f6b47280d0cbad93d
Roman Parashchenko <romikoops1@gmail.com>
2016-07-04 13:46:07 +0200

    implemented new tests for dynamic methods

--- spec/unit/lib/web/section_dsl_spec.rb
+++ spec/unit/lib/web/section_dsl_spec.rb
@@ -41,5 +41,46 @@ RSpec.describe Howitzer::Web::SectionDsl do
     end
   end
 
-  describe 'dynamic_methods'
+  describe 'dynamic_methods' do
+    context 'when section with single argument' do
+      let(:web_page_object) { web_page_class.new }
+      let(:session) { double(:session) }
+      before do
+        allow(Capybara).to receive(:current_session) { session }
+        web_page_class.class_eval do
+          section :foo
+        end
+      end
+      describe '#name_section' do
+        let(:capybara_element) { double }
+        subject { web_page_object.send(:foo_section) }
+        before { expect(session).to receive(:find).with(:xpath, './/div') { capybara_element } }
+        it { is_expected.to be_a(section_class) }
+      end
+      describe '#name_sections' do
+        subject { web_page_object.send(:foo_sections) }
+        let(:capybara_element1) { double }
+        let(:capybara_element2) { double }
+        before do
+          expect(session).to receive(:all).with(:xpath, './/div') { [capybara_element1, capybara_element2] }
+        end
+        it 'should return collection of sections' do
+          res = subject
+          expect(res.size).to eq(2)
+          expect(res.first).to be_a(section_class)
+          expect(res.last).to be_a(section_class)
+        end
+      end
+      describe '#has_name_section?' do
+        subject { web_page_object.send(:has_foo_section?) }
+        before { expect(session).to receive(:has_selector?).with(:xpath, './/div') { true } }
+        it { is_expected.to eq(true) }
+      end
+      describe '#has_no_name_element?' do
+        subject { web_page_object.send(:has_no_foo_section?) }
+        before { expect(session).to receive(:has_no_selector?).with(:xpath, './/div') { true } }
+        it { is_expected.to eq(true) }
+      end
+    end
+  end
 end
