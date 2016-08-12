71e9687413fae66593f33d296db56f1fabbdaad7
greenhairedlime <kalinichenko1masha@gmail.com>
2016-08-12 21:36:33 +0300

    Update users.rb

--- generators/prerequisites/templates/users.rb
+++ generators/prerequisites/templates/users.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :user do
     email { "u#{Howitzer::Utils::Gen.serial}@#{settings.mailgun_domain}" }
     name { "FirstName LastName #{Howitzer::Utils::Gen.serial}" }
-    password { settings.def_test_pass }
-    password_confirmation { settings.def_test_pass }
+    password { settings.app_test_pass }
+    password_confirmation { settings.app_test_pass }
   end
 end
