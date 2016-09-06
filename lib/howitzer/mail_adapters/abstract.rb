c3dd2998af747df900decac57845475f6ed70594
Roman Parashchenko <romikoops1@gmail.com>
2016-09-06 13:42:21 +0200

    documented mail adapters

--- lib/howitzer/mail_adapters/abstract.rb
+++ lib/howitzer/mail_adapters/abstract.rb
@@ -1,93 +1,70 @@
 module Howitzer
   module MailAdapters
-    # This class holds abstract mail adapter methods
+    # +Abstract+ is the superclass of all mail adapters.
+    #
+    # @abstract This class should not be used directly. Instead, create a
+    #  subclass that implements:
+    #  {.find}
+    #  {#plain_text_body}
+    #  {#html_body}
+    #  {#text}
+    #  {#mail_from}
+    #  {#recipients}
+    #  {#received_time}
+    #  {#sender_email}
+    #  {#mime_part}
     class Abstract
       attr_reader :message
 
+      # Finds an email in mailbox
+      # @param recipient [String] email
+      # @param subject [String]
       def self.find(_recipient, _subject)
         raise NotImplementedError
       end
 
-      ##
-      #
-      # Creates new email with message
-      #
-      # *Parameters:*
-      # * +message+ - email message
-      #
-
+      # Creates a new instance of email
+      # @param message [Object] original message data
       def initialize(message)
         @message = message
       end
 
-      ##
-      #
       # Returns plain text body of email message
-      #
-
       def plain_text_body
         raise NotImplementedError
       end
 
-      ##
-      #
       # Returns html body of email message
-      #
-
       def html_body
         raise NotImplementedError
       end
 
-      ##
-      #
       # Returns mail text
-      #
-
       def text
         raise NotImplementedError
       end
 
-      ##
-      #
       # Returns who has send email data in format: User Name <user@email>
-      #
-
       def mail_from
         raise NotImplementedError
       end
 
-      ##
-      #
       # Returns array of recipients who has received current email
-      #
-
       def recipients
         raise NotImplementedError
       end
 
-      ##
-      #
       # Returns email received time in format:
-      #
-
       def received_time
         raise NotImplementedError
       end
 
-      ##
-      #
       # Returns sender user email
-      #
-
       def sender_email
         raise NotImplementedError
       end
 
-      ##
-      #
       # Allows to get email MIME attachment
-      #
-
       def mime_part
         raise NotImplementedError
       end
--- lib/howitzer/mail_adapters/mailgun.rb
+++ lib/howitzer/mail_adapters/mailgun.rb
@@ -6,6 +6,10 @@ module Howitzer
   module MailAdapters
     # This class represents Mailgun mail adapter
     class Mailgun < Abstract
+      # Finds an email in storage
+      # @note emails are stored for 3 days only!
+      # @param recipient [String] email
+      # @param subject [String]
       def self.find(recipient, subject)
         message = {}
         retryable(find_retry_params) { message = retrieve_message(recipient, subject) }
@@ -14,6 +18,54 @@ module Howitzer
           "Message with subject '#{subject}' for recipient '#{recipient}' was not found."
         ) if message.blank?
         new(message)
+      end
+
+      # @return [String] plain text body of email message
+      def plain_text_body
+        message['body-plain']
+      end
+
+      # @return [String] html body of email message
+      def html_body
+        message['stripped-html']
+      end
+
+      # @return [String] stripped text
+      def text
+        message['stripped-text']
+      end
+
+      # @return [String] email specified in `From` field
+      def mail_from
+        message['From']
+      end
+
+      # @return [String] recipient emails separated with `, `
+      def recipients
+        message['To'].split ', '
+      end
+
+      # @return [String]
+      def received_time
+        message['Received'][/\w+, \d+ \w+ \d+ \d+:\d+:\d+ -\d+ \(\w+\)$/]
+      end
+
+      # @return [String] real sender email
+      def sender_email
+        message['sender']
+      end
+
+      # @return [Array] attachments
+      def mime_part
+        message['attachments']
+      end
+
+      # @raise [NoAttachmentsError] if no attachments present
+      # @return [Array] attachments
+      def mime_part!
+        files = mime_part
+        return files if files.present?
+        Howitzer::Log.error NoAttachmentsError, 'No attachments where found.'
       end
 
       def self.events
@@ -49,43 +101,6 @@ module Howitzer
         MailgunApi::Connector.instance.client.get_url(message_url).to_h
       end
       private_class_method :retrieve_message
-
-      def plain_text_body
-        message['body-plain']
-      end
-
-      def html_body
-        message['stripped-html']
-      end
-
-      def text
-        message['stripped-text']
-      end
-
-      def mail_from
-        message['From']
-      end
-
-      def recipients
-        message['To'].split ', '
-      end
-
-      def received_time
-        message['Received'][/\w+, \d+ \w+ \d+ \d+:\d+:\d+ -\d+ \(\w+\)$/]
-      end
-
-      def sender_email
-        message['sender']
-      end
-
-      def mime_part
-        files = message['attachments']
-        if files.blank?
-          Howitzer::Log.error NoAttachmentsError, 'No attachments where found.'
-          return
-        end
-        files
-      end
     end
   end
 end
