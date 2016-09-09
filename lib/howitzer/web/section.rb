1788021ab04be29d2939a017aeb6c46d8a3d18f3
Roman Parashchenko <romikoops1@gmail.com>
2016-09-09 23:43:50 +0200

    documented code

--- lib/howitzer/web/section.rb
+++ lib/howitzer/web/section.rb
@@ -2,10 +2,20 @@ require 'howitzer/web/base_section'
 
 module Howitzer
   module Web
-    # describe me later!
+    # This class uses for named sections which possible to reuse in different pages
     class Section < BaseSection
       class << self
         protected
+
+        # DSL method which specifies section container selector represented by HTML element.
+        # Any elements described in sections will start in this HTML element.
+        # @param args [Array] original Capybara arguments. For details, see `Capybara::Node::Finders#all.
+        # @raise [ArgumentError] if no arguments were passed
+        # @example
+        #   class MenuSection < Howitzer::Web::Section
+        #     me :xpath, ".//*[@id='panel']"
+        #   end
+        # @!visibility public
 
         def me(*args)
           raise ArgumentError, 'Finder arguments are missing' if args.blank?
--- lib/howitzer/web/section_dsl.rb
+++ lib/howitzer/web/section_dsl.rb
@@ -6,6 +6,9 @@ module Howitzer
         base.extend(ClassMethods)
       end
 
+      # Returns capybara context. For example, capybara session, parent element, etc.
+      # @abstract should be defined in parent context
+
       def capybara_context
         raise NotImplementedError, "Please define 'capybara_context' method for class holder"
       end
@@ -15,6 +18,8 @@ module Howitzer
         # This class is for private usage only
         class SectionScope
           attr_accessor :section_class
+
+          # Instantiates anynomous or named section and executes block code in section scope
 
           def initialize(name, *args, &block)
             @args = args
@@ -27,13 +32,24 @@ module Howitzer
             instance_eval(&block) if block_given?
           end
 
+          # Defines element on section
+          # @see Howitzer::PageDsl::ClassMethods#element
+
           def element(*args)
             section_class.send(:element, *args)
           end
 
+          # Delegates section describing to section class
+
           def section(name, *args, &block)
             section_class.send(:section, name, *args, &block)
           end
+
+          # Returns selector for the section.
+          # @note If anonymous section uses, then inline selector should be specified.
+          #   Otherwise arguments should be defined with `.me` dsl method in named section
+          # @return [Array]
+          # @raise [ArgumentError] when finder arguments were not specified
 
           def finder_args
             @finder_args ||= begin
@@ -45,8 +61,46 @@ module Howitzer
 
         protected
 
-        # TODO: describe me
+        # DSL method which defines named or anonymous section within page or section
+        # @note This method generates following dynamic methods:
         #
+        #   <b><em>section_name</em>_section</b> - equals capybara #find(...) method
+        #
+        #   <b><em>section_name</em>_sections</b> - equals capybara #all(...) method
+        #
+        #   <b><em>section_name</em>_sections.first</b> - equals capybara #first(...) method
+        #
+        #   <b>has_<em>section_name</em>_section?</b> - equals capybara #has_selector(...) method
+        #
+        #   <b>has_no_<em>section_name</em>_section?</b> - equals capybara #has_no_selector(...) method
+        # @param name [Symbol, String] unique section name
+        # @param args [Array] original Capybara arguments. For details, see `Capybara::Node::Finders#all.
+        #  (In most cases should be ommited for named sections because the section selector is specified
+        #  with `#me` method. But must be specified for anonymous sections)
+        # @param block [Proc] this block can contain nested sections and elements
+        # @example Named section
+        #   class MenuSection < Howitzer::Web::Section
+        #     me :xpath, ".//*[@id='panel']"
+        #   end
+        #   class HomePage < Howitzer::Web::Page
+        #     section :menu
+        #   end
+        #   HomePage.on { is_expected.to have_menu_section }
+        # @example Anonymous section
+        #   class HomePage < Howitzer::Web::Page
+        #     section :info_panel, '#panel' do
+        #       element :edit_button, '.edit'
+        #       element :title_field, '.title'
+        #
+        #       def edit_info(title: nil)
+        #         edit_button_element.click
+        #         title_field_element.set(title)
+        #       end
+        #     end
+        #   end
+        #   HomePage.on { info_panel.edit_info(title: 'Test Title') }
+        # @!visibility public
+
         def section(name, *args, &block)
           scope = SectionScope.new(name, *args, &block)
           define_section_method(scope.section_class, name, scope.finder_args)
