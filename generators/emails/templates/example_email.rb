c8839c5a4654a3fe3be073b40569c3a215be6a53
lecik <lec1k2103@gmail.com>
2017-06-09 12:23:49 +0300

    method refactoring

--- generators/emails/templates/example_email.rb
+++ generators/emails/templates/example_email.rb
@@ -3,6 +3,6 @@ class TestEmail < Howitzer::Email
   subject 'Test email'
 
   def addressed_to?(new_user)
-    plain_text_body.match?(/Hi #{new_user}/)
+    plain_text_body =~ /Hi #{new_user}/ ? true : false
   end
 end
