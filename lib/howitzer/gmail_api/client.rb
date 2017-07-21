303a07945335c3c7c24a25898318457c49dab87e
lecik <lec1k2103@gmail.com>
2017-07-21 15:56:23 +0300

    gmail client correction

--- lib/howitzer/gmail_api/client.rb
+++ lib/howitzer/gmail_api/client.rb
@@ -4,9 +4,8 @@ module Howitzer
   module GmailApi
     # A GmailApi::Client object is used to communicate with the Gmail API.
     class Client
-
       def initialize
-        @@client ||= Gmail.connect(Howitzer.gmail_login, Howitzer.gmail_password)
+        @client = Gmail.connect(Howitzer.gmail_login, Howitzer.gmail_password)
       end
 
       # Finds message according to given parameters
@@ -16,9 +15,8 @@ module Howitzer
       # @return [Gmail::Message] gmail message object
 
       def find_message(recipient, subject)
-        @@client.inbox.emails(to: recipient, subject: subject).last
+        @client.inbox.emails(to: recipient, subject: subject).last
       end
-
     end
   end
 end
