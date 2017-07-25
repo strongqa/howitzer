3cc4eab2a47230ded4c6477e1d0e48096a391466
lecik <lec1k2103@gmail.com>
2017-07-25 11:57:35 +0300

    obsolete code removed

--- generators/config/templates/boot.rb
+++ generators/config/templates/boot.rb
@@ -1,6 +1,5 @@
 require 'rubygems'
 require 'bundler/setup'
-require 'howitzer/gmail_api/client'
 
 Bundler.require(:default)
 
@@ -11,7 +10,5 @@ Dir[
   './prerequisites/models/**/*.rb',
   './prerequisites/factory_girl.rb'
 ].each { |f| require f }
-
-Howitzer::GmailApi::Client.new if Howitzer.mail_adapter == 'gmail'
 
 String.send(:include, Howitzer::Utils::StringExtensions)
--- generators/tasks/templates/common.rake
+++ generators/tasks/templates/common.rake
@@ -1,7 +1,1 @@
 # Put here any common rake tasks
-require 'howitzer/gmail_api/client'
-
-desc 'Provides url to authorize application for gmail API usage'
-task :'gmail-init' do
-  Howitzer::GmailApi::Client.new
-end
