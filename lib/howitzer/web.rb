5cd3083dbdcda3e4e034043ebd0fcd204c358242
nataliya-m <chagrin2013@gmail.com>
2017-11-24 17:10:39 +0100

    added documentation for new methods

--- lib/howitzer/web.rb
+++ lib/howitzer/web.rb
@@ -1,13 +1,21 @@
 module Howitzer
   # This class holds everything related with web GUI
   module Web
+    # method which specifies to work with a given session
+    # @param name [String, Symbol] a session name
+
     def self.within_session(name, &block)
       Capybara.using_session(name, &block)
     end
 
+    # @return [String, nil] a current session name
+
     def self.current_session_name
       Capybara.session_name
     end
+
+    # Specifies a session with which need to work now
+    # @param value [String, Symbol] a session name
 
     def self.current_session_name=(value)
       Capybara.session_name = value
