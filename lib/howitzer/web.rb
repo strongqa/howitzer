a679804473bb42621ee44b44894aa27d149d6806
nataliya-m <chagrin2013@gmail.com>
2017-11-24 16:34:02 +0100

    added methods for sessions control

--- lib/howitzer/web.rb
+++ lib/howitzer/web.rb
@@ -1,6 +1,17 @@
 module Howitzer
   # This class holds everything related with web GUI
   module Web
+    def self.within_session(name, &block)
+      Capybara.using_session(name, &block)
+    end
+
+    def self.current_session_name
+      Capybara.session_name
+    end
+
+    def self.current_session_name=(value)
+      Capybara.session_name = value
+    end
   end
 end
 require 'howitzer/web/page_validator'
