76fa8fef6a75c53c622d28de157887f505bd3538
Tymofii Mikriukov <tmikriukov@strongsd.com>
2022-07-08 13:24:02 +0300

    Selenium W3C compatibility

--- spec/unit/lib/capybara_helpers_spec.rb
+++ spec/unit/lib/capybara_helpers_spec.rb
@@ -637,22 +637,24 @@ RSpec.describe Howitzer::CapybaraHelpers do
       allow(self).to receive(:prefix_name) { 'Suite' }
     end
 
-    if Gem::Requirement.new(['>=3', '<4'])
-                       .satisfied_by?(Gem::Version.new(Selenium::WebDriver::VERSION))
-      it 'should return correct hash' do
-        is_expected.to eq(platform: 'Windows',
-                          browserName: 'Safari',
-                          version: '10.0',
-                          name: 'Suite Safari')
-      end
-    elsif Gem::Requirement.new(['>=4', '<5'])
-                          .satisfied_by?(Gem::Version.new(Selenium::WebDriver::VERSION))
-      it 'should return correct hash' do
-        is_expected.to eq(platformName: 'Windows',
-                          browserName: 'Safari',
-                          browserVersion: '10.0',
-                          name: 'Suite Safari')
-      end
+    it 'should return correct hash' do
+      is_expected.to eq(platform: 'Windows',
+                        browserName: 'Safari',
+                        version: '10.0',
+                        name: 'Suite Safari')
+    end
+  end
+
+  describe '.required_w3c_cloud_caps' do
+    subject { required_w3c_cloud_caps }
+    before do
+      allow(Howitzer).to receive(:cloud_browser_name) { 'Safari' }
+      allow(Howitzer).to receive(:cloud_browser_version) { '10.0' }
+    end
+
+    it 'should return correct hash' do
+      is_expected.to eq(browserName: 'Safari',
+                        browserVersion: '10.0')
     end
   end
 
